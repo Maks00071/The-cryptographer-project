6ac07d13b79df97050a9d7af70c1c9f5a0db4d3b
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:42:37 +0300

    CHANGE Class MainApp

--- src/src/main/java/MainApp.java
+++ src/src/main/java/MainApp.java
@@ -7,5 +7,6 @@
 public class MainApp {
     public static void main(String[] args) {
         // Логика для выбора режима работы, вызов соответствующих методов
+        System.out.println("Начало работы!");
     }
 }
