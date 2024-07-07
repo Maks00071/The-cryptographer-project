478191f1b6bbc1590a2504fbf7b3905cf48875b0
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:45:35 +0300

    ADD Class BruteForce

---
+++ src/src/main/java/BruteForce.java
@@ -0,0 +1,11 @@
+/**
+ * Класс реализует метод перебора всех ключей для взлома
+ */
+
+public class BruteForce {
+
+    public String decryptByBruteForce(String encryptedText, char[] alphabet) {
+        // Логика brute force
+        return null;
+    }
+}
