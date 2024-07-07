0ad851f0bbcd170c9f630baf007e458547121fe7
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:48:23 +0300

    ADD Class StatisticalAnalyzer

---
+++ src/src/main/java/StatisticalAnalyzer.java
@@ -0,0 +1,11 @@
+/**
+ * Класс для статистического анализа при расшифровке
+ */
+
+public class StatisticalAnalyzer {
+
+    public int findMostLikelyShift(String encryptedText, char[] alphabet, String representativeText) {
+        // Логика статистического анализа для определения сдвига
+        return 1;
+    }
+}
