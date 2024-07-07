299b5e503272d768e8aa5b7c621ec5a7913eb172
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:34:16 +0300

    ADD Class FileManager

---
+++ src/src/main/java/FileManager.java
@@ -0,0 +1,15 @@
+/**
+ * Класс отвечает за чтение и запись файлов
+ */
+
+public class FileManager {
+
+    public String readFile(String filePath) {
+        // Логика чтения файла
+        return null;
+    }
+
+    public void writeFile(String content, String filePath) {
+        // Логика записи файла
+    }
+}
