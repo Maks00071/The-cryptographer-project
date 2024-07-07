4b085f3a23b11fd0570af53097dd82f38c17740f
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:31:08 +0300

    ADD Class Cipher

---
+++ src/src/main/java/Cipher.java
@@ -0,0 +1,54 @@
+/**
+ * Класс, реализующий функциональность шифра Цезаря и дешифровки
+ */
+
+public class Cipher {
+    private char[] alphabet;
+
+    public Cipher(char[] alphabet) {
+        this.alphabet = alphabet;
+    }
+
+    /**
+     * Метод, выполняющий шифрование текста
+     * @param text - текст, который необходимо зашифровать
+     * @param shift - ключ шифрования (параметр сдвига)
+     * @return - зашифрованный текст
+     */
+    public String encrypt(String text, int shift) {
+        // Логика шифрования
+        return null;
+    }
+
+    /**
+     * Метод, выполняющий расшифровывание требуемого текста
+     * @param encryptedText - текст, который необходимо расшифровать
+     * @param shift - ключ расшифрования (параметр сдвига)
+     * @return - расшифрованный текст
+     */
+    public String decrypt(String encryptedText, int shift) {
+        // Логика расшифровки
+        return null;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
