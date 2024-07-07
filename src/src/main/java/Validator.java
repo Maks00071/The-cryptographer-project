c8b67c07c86507bd6ad95976f07ddad57acba16e
Maks Korystov <corystov.maks@yandex.ru>
2024-07-07 18:38:14 +0300

    ADD Class Validator

---
+++ src/src/main/java/Validator.java
@@ -0,0 +1,18 @@
+/**
+ * Класс, отвечающий за валидацию входных данных, таких как:
+ *  - существование файла
+ *  - допустимость ключа
+ */
+
+public class Validator {
+
+    public boolean isValidKey(int key, char[] alphabet) {
+        // Проверка ключа
+        return true;
+    }
+
+    public boolean isFileExists(String filePath) {
+        // Проверка существования файла
+        return true;
+    }
+}
